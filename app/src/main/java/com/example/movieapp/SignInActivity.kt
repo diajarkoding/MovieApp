10ac07a60bbd2c9540f2655fad5fa220f63ffbff
Iskandar Muhaemin <iskandar.muhaemin@danasaham.co.id>
2024-07-09 10:31:27 +0700

    setup firebase

--- app/src/main/java/com/example/movieapp/SignInActivity.kt
+++ app/src/main/java/com/example/movieapp/SignInActivity.kt
@@ -1,11 +1,14 @@
 package com.example.movieapp
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.movieapp.R
+import com.google.firebase.Firebase
+import com.google.firebase.database.database
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +19,14 @@ class SignInActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnSignIn = findViewById<Button>(R.id.btn_sign_in)
+        btnSignIn.setOnClickListener {
+            val database = Firebase.database("https://auth-demo-flutter-917eb-default-rtdb.asia-southeast1.firebasedatabase.app")
+            val myRef = database.getReference("message")
+
+            myRef.setValue("Hello, World!")
         }
     }
 }
